57618df5b60bb1efbda765d54229cec6915e0236
Елена Василькова <vasilkovaea@gmail.com>
2025-11-02 22:16:41 +0300

    Реализована функция draw_gallows

--- hangman.py
+++ hangman.py
@@ -1,40 +1,142 @@
-from random import randint
+from random import choice
 
 
-def word() -> str:
-    pass
+def get_word() -> str:
+    """Получения слова для игры"""
+    # распаковка файла
+    word_list = ["апельсин", "библиотека", "вертолёт", "галактика", "динозавр",
+    "ежевика", "жаворонок", "зоопарк", "ископаемое", "календарь",
+    "лаборатория", "магистраль", "небоскрёб", "олимпиада", "панорама",
+    "репетитор", "самолёт", "телескоп", "университет", "фестиваль"]
+    return choice(word_list)
 
 
 def is_valid_letter(letter: str) -> bool:
+    """Проверка введеного символа"""
     pass
 
 
 def is_valid_answer(answer: str) -> bool:
+    """Проверка валидности ответа (да /нет)"""
     pass
 
 
-def is_letter_in_word(letter: str) -> bool:
+def is_letter_in_word(letter: str, word: str) -> bool:
+    """Проверка наличия буквы в слове"""
     pass
 
 
-def drawing_of_gallows(number_of_errors: int) -> str:
-    pass
+def draw_gallows(errors_count: int) -> str:
+    """Отрисовка виселицы в зависимости от количества ошибок"""
+    match errors_count:
+        case 0:
+            print("""
+           ------
+           |    |
+           |
+           |
+           |
+           |
+           |
+        --------
+        """)
+        case 1:
+            print("""
+            ------
+            |    |
+            |    O
+            | 
+            | 
+            | 
+            |
+            |
+        --------
+        """)
+        case 2:
+            print("""
+            ------
+            |    |
+            |    O
+            |    |
+            |    |
+            | 
+            |
+            |
+        --------
+        """)
+        case 3:
+            print("""
+           ------
+           |    |
+           |    O
+           |   /|
+           |    |
+           | 
+           |
+           |
+        --------
+        """)
+        case 4:
+            print("""
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |    |
+           |   
+           |
+           |
+        --------
+        """)
+        case 5:
+            print("""
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |    |
+           |   / 
+           |
+           |
+        --------
+        """)
+        case 6:
+            print(""""
+            ------
+            |    |
+            |    O
+            |   /|\\
+            |    |
+            |   / \\
+            |
+            |
+        --------
+            """)
+
+    print(f'Количество ошибок: {errors_count}')
 
 
-def output_of_result(letter: str):
-    number_of_errors: int = 0
+def processing_guess(letter: str, word: str):
+    """Обработка угадывания слова"""
+    errors_count: int = 0
     is_valid_letter(letter)     # валидация буквы
-    is_letter_in_word(letter)  # проверка наличия буквы в слове
-    print('Счетчик ошибок')
-    print('Текущее состояние висилицы') # ASCII
+
+    if is_letter_in_word(letter, word):
+        errors_count += 1
+
+    draw_gallows(errors_count)
 
 
 print('Описание игры')
 
 while True:
+    word: str = get_word()
+    letters_count: int = len(word)
+    hidden_word = '*' * letters_count
+    print(hidden_word)
     print('Запрос ввода буквы')
     letter: str = input()
-    output_of_result(letter)
+    processing_guess(letter, word)
     print('Сыграем ещё раз? (да / нет)')
     answer: str = input()
     is_valid_answer(answer)
