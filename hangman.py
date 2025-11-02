46fd9cea3991fba1799badc12a5df52462deaa7a
Елена Василькова <vasilkovaea@gmail.com>
2025-11-02 23:55:29 +0300

    Исправлена игровая логика, добавлен счетчик оставшихся попыток, словарь введёных букв и словарь введёных слов.

--- hangman.py
+++ hangman.py
@@ -13,7 +13,8 @@ def get_word() -> str:
 
 def is_valid_letter(letter: str) -> bool:
     """Проверка введеного символа"""
-    if letter
+    # проверить, что введеный символ - буква
+    # проверить, вводилась ли буква ранее
 
 
 def is_valid_answer(answer: str) -> bool:
@@ -21,12 +22,22 @@ def is_valid_answer(answer: str) -> bool:
     pass
 
 
+def is_valid_word(letter: str) -> bool:
+    """Проверка введеного символа"""
+    # проверить, вводилось ли слово ранее
+
+
 def is_letter_in_word(letter: str, word: str) -> bool:
     """Проверка наличия буквы в слове"""
-    pass
+    if is_valid_letter(letter):
+        # проверяем наличие буквы в слове
+        pass
+    else:
+        # просим повторно ввести букву
+        pass
 
 
-def draw_gallows(errors_count: int) -> str:
+def draw_gallows(errors_count: int, guessed_letters: list, guessed_words: list) -> str:
     """Отрисовка виселицы в зависимости от количества ошибок"""
     match errors_count:
         case 0:
@@ -113,23 +124,42 @@ def draw_gallows(errors_count: int) -> str:
         --------
             """)
 
-    print(f'Количество ошибок: {errors_count}')
+    print(f'''
+        Количество ошибок: {errors_count}.
+        Названные буквы: {guessed_letters}.
+        Названные слова: {guessed_words}.
+        ''')
 
 
-def processing_guess(letter: str, word: str):
+
+def processing_guess(letter: str, word: str, guessed_letters: list, guessed_words: list):
     """Обработка угадывания слова"""
     errors_count: int = 0
-    is_valid_letter(letter)     # валидация буквы
 
-    if is_letter_in_word(letter, word):
+    if len(letter) == 1:
+        if not is_letter_in_word(letter, word):
+            errors_count += 1
+            guessed_letters.append(letter)
+        else:
+            pass
+            # открываем буквы
+    elif letter == word:
+        return True
+    elif len(letter) > 1 and letter != word:
         errors_count += 1
+        guessed_words.append(letter)
 
-    draw_gallows(errors_count)
+    draw_gallows(errors_count, guessed_letters, guessed_words)
+    return False
 
 
 def game():
+    guessed: bool = False
+    guessed_letters: list = []
+    guessed_words: list = []
+    tries = 6
 
-    while True:
+    while tries > 0 and not guessed:
         word: str = get_word()
         letters_count: int = len(word)
         hidden_word = '*' * letters_count
@@ -138,19 +168,25 @@ def game():
         ''')
 
         print(f'''
-        У тебя 6 попыток.
+        У тебя {tries} попыток.
         Введи одну букву или слово целиком.
-            ''')  # Добавить подсчет количества оставшихся попыток
+            ''')
+        tries -= 1
         letter: str = input().upper()
-        processing_guess(letter, word)
+        guessed = processing_guess(letter, word, guessed_letters, guessed_words)
 
-        print('Сыграем ещё раз? (да / нет)')
+        if guessed is True:
+            # Выводим результат
+            break
+
+        print('Сыграем ещё раз? (да / нет)\n')
         answer_game: str = input()
         is_valid_answer(answer_game)
 
-        if answer == 'да':
+        if answer_game == 'да':
+            tries = 6
             continue
-        else:
+        elif answer_game:
             break
 
 
@@ -167,4 +203,4 @@ if __name__ == '__main__':
 
     game() if answer == 'да' else print()
 
-    print('Спасибо за игру!')
+    print('\nСпасибо за игру!')
